f46ac0c6d9ae9ea67346b56d742ff34716bbe6fb
sakil <sakiluddin.su@gmail.com>
2021-12-19 09:38:54 +0000

    Initial commit

---
+++ Game5_Puzzle/cfg.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 30, 2021
+
+@author: martinyanev
+'''
+import os
+
+SCREENSIZE = (640, 640)
+
+PICTURE_ROOT_DIR = os.path.join(os.getcwd(), 'C:\\Users\\sakil\\PycharmProjects\\puzzlegame\\resources/pictures')
+
+FONTPATH = os.path.join(os.getcwd(), 'C:\\Users\\sakil\\PycharmProjects\\puzzlegame\\resources/pictures')
+
+BACKGROUNDCOLOR = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+FPS = 40
+
+RANDNUM = 100
